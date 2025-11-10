e128cda3795aed30d5b1303090646281a74bcde7
Tong <cnlutong@gmail.com>
2025-11-10 21:00:46 +0100

    删除旧版本开始重新设计和构建项目

--- controller/application/__init__.py
+++
@@ -1,7 +0,0 @@
-"""应用服务层模块"""
-from .event_bus import EventBus
-from .device_service import DeviceService
-from .scene_service import SceneService
-
-__all__ = ["EventBus", "DeviceService", "SceneService"]
-
--- controller/infrastructure/hal/__init__.py
+++
@@ -1,6 +0,0 @@
-"""HAL 适配层模块"""
-from .client import HALClient
-from .models import HALDeviceState
-
-__all__ = ["HALClient", "HALDeviceState"]
-
